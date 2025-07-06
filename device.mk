6de40f831dfdc5af3b758bc38980e0a0423ee95a
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-06 22:07:51 +0000

    mona: Create RFS MSM MPSS symlink
    Change-Id: I61c5c376b430bd76917932da6ddc987f19994761

--- device.mk
+++ device.mk
@@ -251,6 +251,10 @@ PRODUCT_PACKAGES += \
 # Radio
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    
+# RFS MSM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_mpss_readonly_vendor_fsg_symlink
 
 # Screen
 TARGET_SCREEN_DENSITY := 450
