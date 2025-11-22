354f0b6dbf83403a469f903a46a4a26cd48a0728
sabrina010209 <sebidk700@gmail.com>
2025-11-22 17:24:08 -0500

    mona: Set up USB
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Alexander Koskovich <akoskovich@pm.me>

--- device.mk
+++ device.mk
@@ -201,6 +201,21 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
     
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+    
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
