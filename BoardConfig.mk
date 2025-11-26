1c26ab86dea862a3c8f734ac07b83517632fb659
sabrina010209 <sebidk700@gmail.com>
2025-11-25 20:51:21 -0500

    mona: Initial manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,12 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 # Vintf
 PRODUCT_ENFORCE_VINTF_MANIFEST := false
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_parrot.xml
+
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
