726b7f551af4adc5cbc684adc21d36047c51843f
MD Raza <delightreza@gmail.com>
2026-03-29 17:47:15 -0400

    mona: Add touchscreen modules to recovery
    Fixes touch in recovery
    
    Signed-off-by: MD Raza <delightreza@gmail.com>
    Change-Id: I098134ab95d953a61e08493adb045d1d9f05cfa4

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+        
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.touch.rc:recovery/root/init.recovery.touch.rc
 
 # Overlay
 PRODUCT_PACKAGES += \
